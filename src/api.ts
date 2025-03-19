1f36ca40a0e17ce3d41530e47c9d9312b0af0a5b
qhy040404 <qhy040404@163.com>
2025-03-19 17:21:07 +0800

    fix crypto enc data size invalid

--- src/api.ts
+++ src/api.ts
@@ -1,24 +1,23 @@
-import { invoke } from "./tauri.ts";
+import { invoke } from './tauri.ts';
 
 // @ts-expect-error crypto will be there
-import crypto from "crypto";
+import crypto from 'crypto';
 
-const PUBLIC_KEY =
-  "-----BEGIN PUBLIC KEY-----\n" +
-  "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA5W2SEyZSlP2zBI1Sn8Gd\n" +
-  "TwbZoXlUGNKyoVrY8SVYu9GMefdGZCrUQNkCG/Np8pWPmSSEFGd5oeug/oIMtCZQ\n" +
-  "NOn0drlR+pul/XZ1KQhKmj/arWjN1XNok2qXF7uxhqD0JyNT/Fxy6QvzqIpBsM9S\n" +
-  "7ajm8/BOGlPG1SInDPaqTdTRTT30AuN+IhWEEFwT3Ctv1SmDupHs2Oan5qM7Y3uw\n" +
-  "b6K1rbnk5YokiV2FzHajGUymmSKXqtG1USZzwPqImpYb4Z0M/StPFWdsKqexBqMM\n" +
-  "mkXckI5O98GdlszEmQ0Ejv5Fx9fR2rXRwM76S4iZTfabYpiMbb4bM42mHMauupj6\n" +
-  "9QIDAQAB\n" +
-  "-----END PUBLIC KEY-----\n";
+const PUBLIC_KEY = `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA5W2SEyZSlP2zBI1Sn8Gd
+TwbZoXlUGNKyoVrY8SVYu9GMefdGZCrUQNkCG/Np8pWPmSSEFGd5oeug/oIMtCZQ
+NOn0drlR+pul/XZ1KQhKmj/arWjN1XNok2qXF7uxhqD0JyNT/Fxy6QvzqIpBsM9S
+7ajm8/BOGlPG1SInDPaqTdTRTT30AuN+IhWEEFwT3Ctv1SmDupHs2Oan5qM7Y3uw
+b6K1rbnk5YokiV2FzHajGUymmSKXqtG1USZzwPqImpYb4Z0M/StPFWdsKqexBqMM
+mkXckI5O98GdlszEmQ0Ejv5Fx9fR2rXRwM76S4iZTfabYpiMbb4bM42mHMauupj6
+9QIDAQAB
+-----END PUBLIC KEY-----`;
 
 let cachedData: GenericPatchData | null = null;
 let cachedToken: string | null = null;
 
 export async function fetchIsOversea(): Promise<boolean> {
-  return await invoke<boolean>("generic_is_oversea");
+  return await invoke<boolean>('generic_is_oversea');
 }
 
 export async function fetchPatchData(): Promise<GenericPatchData> {
@@ -26,12 +25,12 @@ export async function fetchPatchData(): Promise<GenericPatchData> {
     return cachedData;
   }
 
-  const data = await invoke<GenericPatchData>("generic_get_patch");
+  const data = await invoke<GenericPatchData>('generic_get_patch');
   if (!data) {
-    throw new Error("Failed to fetch patch data");
+    throw new Error('Failed to fetch patch data');
   }
   if (!data.mirrors) {
-    throw new Error("Invalid patch data");
+    throw new Error('Invalid patch data');
   }
   cachedData = data;
   return data;
@@ -39,17 +38,22 @@ export async function fetchPatchData(): Promise<GenericPatchData> {
 
 export async function LoginHomaPassport(
   username: string,
-  password: string
+  password: string,
 ): Promise<boolean> {
-  const req: HomaPassportLoginReq = { UserName: encrypt(username), Password: encrypt(password) };
-  const res = await invoke<HomaPassportLoginResp>("homa_login", {loginReq: req});
+  const req: HomaPassportLoginReq = {
+    UserName: encrypt(username),
+    Password: encrypt(password),
+  };
+  const res = await invoke<HomaPassportLoginResp>('homa_login', {
+    loginReq: req,
+  });
   if (res.retcode === 0) {
     cachedToken = res.data ?? null;
     return true;
   }
 
-  await invoke("error_dialog", {
-    title: "登录失败",
+  await invoke('error_dialog', {
+    title: '登录失败',
     message: res.message,
   });
   return false;
@@ -61,10 +65,10 @@ export async function LoadToken(token: string): Promise<void> {
 
 export async function IsCdnAvailable(): Promise<boolean> {
   if (!cachedToken) {
-    throw new Error("Not logged in");
+    throw new Error('Not logged in');
   }
 
-  const userinfo = await invoke<HomaPassportUserInfo>("homa_fetch_userinfo", {
+  const userinfo = await invoke<HomaPassportUserInfo>('homa_fetch_userinfo', {
     token: cachedToken,
   });
 
@@ -77,28 +81,33 @@ export async function IsCdnAvailable(): Promise<boolean> {
 
 export async function GetCdnUrl(): Promise<string> {
   if (!cachedToken) {
-    throw new Error("Not logged in");
+    throw new Error('Not logged in');
   }
 
   if (!cachedData) {
-    throw new Error("Patch data not fetched");
+    throw new Error('Patch data not fetched');
   }
 
-  const filename = cachedData.urls[0].split("/").pop();
-  return await invoke<string>("homa_fetch_cdn", {
+  const filename = cachedData.urls[0].split('/').pop();
+  return await invoke<string>('homa_fetch_cdn', {
     token: cachedToken,
     filename: filename,
   });
 }
 
 function encrypt(data: string): string {
-  const encData = crypto.publicEncrypt(
-    {
-      key: PUBLIC_KEY,
-      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-    },
-    // @ts-expect-error And Buffer will be there
-    Buffer.from(data)
-  );
-  return encData.toString("base64");
+  // @ts-expect-error And Buffer will be there
+  let encData = Buffer.alloc(1);
+  while (encData.length !== 256) {
+    encData = crypto.publicEncrypt(
+      {
+        key: PUBLIC_KEY,
+        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      },
+      // @ts-expect-error And Buffer will be there
+      Buffer.from(data),
+    );
+  }
+
+  return encData.toString('base64');
 }
